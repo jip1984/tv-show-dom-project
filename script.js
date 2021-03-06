c691f7793254dc8326bec958a2bb89a49663274c
jon <jonprosser84@gmail.com>
2021-03-06 09:39:29 +0000

    for review

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //You can edit ALL of the code here
 <<<<<<< Updated upstream
 =======
+let root = document.getElementById('root');
+const searchBar = document.getElementById('search');
+const showSearh = document.getElementById('select-show');
+const showEpisode = document.getElementById('select-episode');
+const count = document.getElementById('count');
+const ul = document.createElement('ul');
+let li;
+let allEpisodes;
+let shows;
+// variables
+
 >>>>>>> Stashed changes
 function setup() {
   const allEpisodes = getAllEpisodes();
