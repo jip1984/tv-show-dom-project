d8b89c70f6027ce7fed0f272428350c3c84e3ff3
jon <jonprosser84@gmail.com>
2021-03-06 09:39:21 +0000

    for review

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //You can edit ALL of the code here
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
